5971258b14cef7115704fdf0506ca27a43aeaa8e
aGir18 <46863674+aGir18@users.noreply.github.com>
2022-05-24 07:10:48 +0100

    introduzco ConfiguracionPorJava

---
+++ src/main/java/es/proyecto/animuslucrandi/ConfiguracionPorJava.java
@@ -0,0 +1,42 @@
+package es.proyecto.animuslucrandi;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+@Configuration
+@ComponentScan("es.proyecto.animuslucrandi")
+public class ConfiguracionPorJava {
+
+  @Bean
+  public ObjectMapper getObjectMapper() {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+    return mapper;
+  }
+  
+  @Bean
+  CorsFilter corsFilter() {
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    final CorsConfiguration config = new CorsConfiguration();
+    config.setAllowCredentials(true);
+    config.setAllowedOrigins(Collections.singletonList("*"));
+    config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+    config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsFilter(source);
+  }
+  
+}
