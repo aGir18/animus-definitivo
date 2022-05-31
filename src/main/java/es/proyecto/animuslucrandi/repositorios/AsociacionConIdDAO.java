5fe3af93cc802194d4ad33a7ae022743569e9406
aGir18 <46863674+aGir18@users.noreply.github.com>
2022-05-31 16:36:20 +0100

    introduzco JPA

--- src/main/java/es/proyecto/animuslucrandi/repositorios/AsociacionConIdDAO.java
+++ src/main/java/es/proyecto/animuslucrandi/repositorios/AsociacionConIdDAO.java
@@ -2,10 +2,14 @@ package es.proyecto.animuslucrandi.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import es.proyecto.animuslucrandi.entidades.AsociacionConId;
 
 @RepositoryRestResource(path = "asociaciones", itemResourceRel = "asociacion", collectionResourceRel = "asociaciones")
 public interface AsociacionConIdDAO extends JpaRepository<AsociacionConId, Long> {
 
+  @RestResource(exported = false)
+  void delete(AsociacionConId asociacion);
+  
 }
--- src/main/java/es/proyecto/animuslucrandi/repositorios/NegocioConIdDAO.java
+++ src/main/java/es/proyecto/animuslucrandi/repositorios/NegocioConIdDAO.java
@@ -1,11 +1,23 @@
 package es.proyecto.animuslucrandi.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import es.proyecto.animuslucrandi.entidades.NegocioConId;
 
 @RepositoryRestResource(path = "negocios", itemResourceRel = "negocio", collectionResourceRel = "negocios")
 public interface NegocioConIdDAO extends JpaRepository<NegocioConId, Long>{
 
+  @RestResource(path="por-nombre")
+  List<NegocioConId> findByNombreIgnoreCase(@Param("nombre")String txt);
+    
+  //Si yo NO quisiera borrarlo: 
+  //@RestResource(exported = false)
+  //void delete(Cliente cliente);
+   
+  
 }
